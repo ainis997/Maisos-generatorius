09b01afc9ef125056b9294756d90426b0bcafcde
ainis997 <235386176+ainis997@users.noreply.github.com>
2026-09-16 22:10:18 +0300

    Padaryta pirminė hashavimo versija. Kodas suskaidytas į funkcijas.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,16 @@
 #include <cstdint>
 #include <cstring>
 #include <bit>
+#include <iomanip>
 
 const int ISVESTIES_BAITU_SK = 32;
 
 void spausd_baitais(std::vector<uint8_t> baitai)
 {
+    std::cout << std::hex; // tai lieka kaip nustatymas
     for (uint8_t baitas : baitai)
     {
-        std::cout << std::hex << static_cast<int>(baitas) << " ";
+        std::cout << std::setfill('0') << std::setw(2) << static_cast<int>(baitas) << " ";
     }
     std::cout << '\n';
 }
@@ -26,33 +28,31 @@ void spausd_raidem(std::vector<uint8_t> baitai)
     std::cout << '\n';
 }
 
-int main()
-{
-    std::string investis_str;
-    std::cout << "Invesk tekstu:\n";
-    std::getline(std::cin, investis_str);
-    std::vector<uint8_t> investis(investis_str.begin(), investis_str.end());
-    auto inv_ilgis = investis.size();
-    spausd_baitais(investis);
-    std::cout << '\n';
-    // ======================================================
-    // ===== skaidymas 32 bitų blokuos
+// std::vector<uint8_t> gaut_ivesties_baitus(std::string ivestis)
+// {
+//     return;
+// }
 
-    size_t paddingas = (4 - (inv_ilgis % 4)) % 4; // tam, kad "investis" ilgis būtų 4-ių kartotinis
-    investis.resize(inv_ilgis + paddingas);
+std::vector<uint32_t> baitus_sujungt_po_4(std::vector<uint8_t> baitai)
+{
+    auto inv_ilgis = baitai.size();
+    size_t paddingas = (4 - (inv_ilgis % 4)) % 4; // tam, kad vektoriaus "baitai" ilgis būtų 4-ių kartotinis
+    baitai.resize(inv_ilgis + paddingas);
     std::vector<uint32_t> blokai;
     // reiktų rezervuot iš anksto vektoriui vietą
-    for (size_t i = 0; i < investis.size(); i += 4) // investis.size() visada turėtų būti 4-ių kartotinis (dėl resize'o)
+    for (size_t i = 0; i < baitai.size(); i += 4) // investis.size() visada turėtų būti 4-ių kartotinis (dėl resize'o)
     {
         uint32_t blokas;
-        std::memcpy(&blokas, &investis[i], 4); // 4, nes 32 / 8 = 4 (bloką sudarys 4 baitai)
+        std::memcpy(&blokas, &baitai[i], 4); // 4, nes 32 / 8 = 4 (bloką sudarys 4 baitai)
         blokas = std::byteswap(blokas);
         // std::cout << ":: " << blokas << '\n';
         blokai.push_back(blokas);
     }
-    // =======================================================
-    // ===== MAIŠA
+    return blokai;
+}
 
+std::vector<uint8_t> gaut_256bit_hasha(std::vector<uint32_t> blokai)
+{
     for (auto it = blokai.begin(); it < blokai.end(); ++it)
     {
         auto idx = std::distance(blokai.begin(), it);
@@ -123,10 +123,24 @@ int main()
             isvestis.push_back(*it);
         }
     }
-    // =====
+    return isvestis;
+}
 
-    spausd_baitais(isvestis);
-    spausd_raidem(isvestis);
+int main()
+{
+    std::string investis_str;
+    std::cout << "Invesk tekstu:\n";
+    std::getline(std::cin, investis_str);
+    std::vector<uint8_t> investis(investis_str.begin(), investis_str.end());
+    auto inv_ilgis = investis.size();
+    spausd_baitais(investis);
+    std::cout << '\n';
+
+    std::vector<uint32_t> blokai = baitus_sujungt_po_4(investis);
+    std::vector<uint8_t> hashas = gaut_256bit_hasha(blokai);
+
+    spausd_baitais(hashas);
+    spausd_raidem(hashas);
 
     return 0;
 }
